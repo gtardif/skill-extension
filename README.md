a75bdc976f3134fcb82da90ce6f79b21ac703932
Christian Dupuis <cd@atomist.com>
2022-09-17 10:33:58 +0200

    Switch to skill package
    Signed-off-by: Christian Dupuis <cd@atomist.com>

---
+++ README.md
@@ -0,0 +1,25 @@
+## Running a Skill in a Docker Desktop Extension
+
+The following files are needed to turn a Docker Desktop extension into a skill:
+
+* `datalog` - datalog subscriptions 
+* `skill.yaml` - skill metadata
+* `Dockerfile` - copying `skill.yaml` and `datalog` into the file system root of the final image; add the `com.docker.skill.api.version="extension/v2"` label
+* Add `@atomist/skill@1.0.0-branch-extension.9` to your dependencies
+
+With these changes in place, you can now call `subscribe` in your Extension UI:
+
+```typescript
+  skill.subscribe({
+    namespace: "atomist",
+    name: "skill-extension",
+    apiKey: "<api key>",
+    workspaceId: "<workspace id>",
+    debug: true,
+  }, {"new_issue": async ctx => {
+    setResponse(JSON.stringify(ctx.event.context.subscription.result[0][0]));
+    return skill.status.completed("Handled new issue event");
+  }}, require("pusher-js/with-encryption"));
+```
+
+In order to register the extension as skill, you need to `docker build` and push the image to your Docker registry and make sure that the image is visible in your Atomist workspace. When we transact the image, we'll discover the skill metadata and register the skill.
--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createDockerDesktopClient } from '@docker/extension-api-client';
 import { Stack, TextField, Typography } from '@mui/material';
 
-import Pusher from 'pusher-js/with-encryption';
+import * as skill from "@atomist/skill";
 
 // Note: This line relies on Docker Desktop's presence as a host application.
 // If you're running this React app in a browser, it won't work properly.
@@ -16,22 +16,16 @@ export function App() {
   const [response, setResponse] = React.useState<string>();
   const ddClient = useDockerDesktopClient();
 
-  Pusher.logToConsole = true;
-
-  var pusher = new Pusher('e7f313cb5f6445399f58', {
-    cluster: 'mt1',
-    channelAuthorization: {
-      endpoint: 'https://api.atomist.com/pusher/channel/auth',
-      headers: {
-        'Authorization': 'Bearer <insert token>'
-      }
-    } as any
-  });
-  
-  var channel = pusher.subscribe('private-encrypted-<workspace id>_atomist_skill-extension');
-  channel.bind('execution-trigger', data => {
-    setResponse(JSON.stringify(data));
-  });
+  skill.subscribe({
+    namespace: "atomist",
+    name: "skill-extension",
+    apiKey: "<api key>",
+    workspaceId: "<workspace id>",
+    debug: true,
+  }, {"new_issue": async ctx => {
+    setResponse(JSON.stringify(ctx.event.context.subscription.result[0][0]));
+    return skill.status.completed("Handled new issue event");
+  }}, require("pusher-js/with-encryption"));
 
   return (
     <>
@@ -39,7 +33,7 @@ export function App() {
       <Stack direction="row" alignItems="start" spacing={2} sx={{ mt: 4 }}>
       <TextField
           label="Subscription event"
-          sx={{ width: 480 }}
+          sx={{ width: "100%" }}
           disabled
           multiline
           variant="outlined"
@@ -50,3 +44,4 @@ export function App() {
     </>
   );
 }
+
